14edbf74af69713470fa2d018e53406ba1b5df04
Hitesh <hitegarg@deloitte.com>
2023-06-07 13:35:23 +0530

    Added KAFKA Producer

---
+++ Hello Producer/src/main/java/org/kafka/example/HelloProducer.java
@@ -0,0 +1,39 @@
+package org.kafka.example;
+
+import org.kafka.example.config.AppConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Properties;
+
+public class HelloProducer {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+        System.out.println("Creating Kafka Producer...");
+        logger.info("Creating Kafka Producer...");
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfig.applicationID);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfig.bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
+
+        logger.info("Start sending messages...");
+        for (int i = 1; i <= AppConfig.numEvents; i++) {
+            producer.send(new ProducerRecord<>(AppConfig.topicName, i, "Simple Message-" + i));
+        }
+
+        logger.info("Finished - Closing Kafka Producer.");
+        producer.close();
+
+    }
+}
---
+++ Hello Producer/src/main/java/org/kafka/example/config/AppConfig.java
@@ -0,0 +1,8 @@
+package org.kafka.example.config;
+
+public class AppConfig {
+    public final static String applicationID = "HelloProducer";
+    public final static String bootstrapServers = "localhost:9092,localhost:9093";
+    public final static String topicName = "hello-producer-topic";
+    public final static int numEvents = 1000000;
+}
